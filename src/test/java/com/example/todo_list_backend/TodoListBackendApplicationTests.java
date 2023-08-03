6a60ca140e173d83715d445ce20f14b84ed0f36b
Harshit Pachori <harshitpachori345@gmail.com>
2023-08-03 20:28:04 +0530

    init push

---
+++ src/test/java/com/example/todo_list_backend/TodoListBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todo_list_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
